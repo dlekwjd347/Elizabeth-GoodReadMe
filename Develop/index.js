ccd5b90d4e7fe3dc668ed93549cff4ad49084ae4
Elizabeth Lee <dlekwjd347@gmail.com>
2020-07-31 21:37:32 -0400

    write rest of prompts to index.md

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,3 @@
-// array of questions for user
-const questions = [
-
-];
-
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -13,7 +7,7 @@ function writeReadme(fileName, readmeData) {
         if (err) {
            return console.log(err)
         }
-        console.log("Added ...")
+        console.log("Readme generated!")
     })
 }
 
@@ -33,114 +27,155 @@ inquirer.prompt([
         }
     }
 },
-// //Description
-//     {type: 'input',
-//     message: "What is your Project Description?",
-//     name: 'description',
-//     validate: description => {
-//         if(description.length < 10){
-//             return "Description is too short.";
-//         }
-//         else{
-//             // all validation checks passed
-//             return true;
-//         }
-//     }
-// },
-// //Table of Contents
-//     {type: 'input',
-//     message: "What is the Table of Contents?",
-//     name: 'conttable',
-//     validate: conttable => {
-//         if(conttable.length < 15){
-//             return "Table of Contents is too short.";
-//         }
-//         else{
-//             // all validation checks passed
-//             return true;
-//         }
-//     }
-// },
-// //Installation
-//     {type: 'input',
-//     message: "What are the steps for Installation?",
-//     name: 'installation',
-//     validate: installation => {
-//         if(installation.length < 5){
-//             return "Installation steps are required.";
-//         }
-//         else{
-//             // all validation checks passed
-//             return true;
-//         }
-//     }
-// },
-// //Usage
-//     {type: 'input',
-//     message: "What is the Usage description?",
-//     name: 'usage',
-//     validate: usage => {
-//         if(usage.length < 10){
-//             return "Usage is too short.";
-//         }
-//         else{
-//             // all validation checks passed
-//             return true;
-//         }
-//     }
-// },
-// //License
-//     {type: 'input',
-//     message: "What is the License?",
-//     name: 'license',
-//     validate: license => {
-//          if(license.length < 3){
-//              return "License required.";
-//     }
-//     else{
-//         // all validation checks passed
-//         return true;
-//     }
-// }
-// },
-// //Contributing
-//     {type: 'input',
-//     message: "Who are the contributors (github usernames)?",
-//     name: 'contributor',
-//     validate: contributor => {
-//          if(contributor.length < 3){
-//              return "Contributor(s) required.";
-//     }
-//     else{
-//         // all validation checks passed
-//         return true;
-//     }
-// }
-// },
-// //Tests
-// //optional
-//     {type: 'input',
-//     message: "What tests are going to be run?",
-//     name: 'test',
+//Description
+    {type: 'input',
+    message: "What is your Project Description?",
+    name: 'description',
+    validate: description => {
+        if(description.length < 10){
+            return "Description is too short.";
+        }
+        else{
+            // all validation checks passed
+            return true;
+        }
+    }
+},
+//Installation
+    {type: 'input',
+    message: "What are the steps for Installation?",
+    name: 'installation',
+    validate: installation => {
+        if(installation.length < 5){
+            return "Installation steps are required.";
+        }
+        else{
+            // all validation checks passed
+            return true;
+        }
+    }
+},
+//Usage
+    {type: 'input',
+    message: "What is the Usage description?",
+    name: 'usage',
+    validate: usage => {
+        if(usage.length < 10){
+            return "Usage is too short.";
+        }
+        else{
+            // all validation checks passed
+            return true;
+        }
+    }
+},
+//License
+    {type: 'input',
+    message: "What is the License?",
+    name: 'license',
+    validate: license => {
+         if(license.length < 3){
+             return "License required.";
+    }
+    else{
+        // all validation checks passed
+        return true;
+    }
+}
+},
+//Contributing
+    {type: 'input',
+    message: "Who are the contributors (github usernames)?",
+    name: 'contributor',
+    validate: contributor => {
+         if(contributor.length < 3){
+             return "Contributor(s) required.";
+    }
+    else{
+        // all validation checks passed
+        return true;
+    }
+}
+},
+//Tests
+//optional
+    {type: 'input',
+    message: "What tests are going to be run?",
+    name: 'test',
 
-// },
-// //Questions 
-// //optional
-// {type: 'input',
-// message: "Any Questions?",
-// name: 'question',
+},
+//Questions 
+//optional
+{type: 'input',
+message: "Any Questions?",
+name: 'question',
 
-// }
+}
 
 
 ]).then( response => {
     console.log(response.title);
     // function to write README file
     let readmeData = "";
-    readmeData += "# Title"
+    readmeData += "# Table of Contents:"
+    readmeData += "\n";
+    readmeData += "## Title"
+    readmeData += "\n";
+    readmeData += "## Description"
+    readmeData += "\n";
+    readmeData += "## Installation Steps"
+    readmeData += "\n";
+    readmeData += "## Usage Description"
+    readmeData += "\n";
+    readmeData += "## License"
+    readmeData += "\n";
+    readmeData += "## Contributors"
+    readmeData += "\n";
+    readmeData += "## Tests"
+    readmeData += "\n";
+    readmeData += "## Questions"
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Title"
     readmeData += "\n";
     readmeData += response.title; // data under heading
-
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Description"
+    readmeData += "\n";
+    readmeData += response.description; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Installation Steps"
+    readmeData += "\n";
+    readmeData += response.installation; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Usage Description"
+    readmeData += "\n";
+    readmeData += response.usage; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## License"
+    readmeData += "\n";
+    readmeData += response.license; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Contributors"
+    readmeData += "\n";
+    readmeData += response.contributor; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Tests"
+    readmeData += "\n";
+    readmeData += response.test; // data under heading
+    readmeData += "\n";
+    readmeData += "\n";
+    readmeData += "## Questions"
+    readmeData += "\n";
+    readmeData += response.question; // data under heading
+    readmeData += "\n";
+    
 
     writeReadme("index.md", readmeData);
 });
--- Develop/index.md
+++ Develop/index.md
@@ -1,2 +1,33 @@
-# Title
-testtttttttttttt
+# Table of Contents:
+## Title
+## Description
+## Installation Steps
+## Usage Description
+## License
+## Contributors
+## Tests
+## Questions
+
+## Title
+qwertyuiop
+
+## Description
+qwertyuiop
+
+## Installation Steps
+qwertyuiop
+
+## Usage Description
+qwertyuiopqwertyuiop
+
+## License
+qwertyuiop
+
+## Contributors
+qwertyuiop
+
+## Tests
+qwertyuiop
+
+## Questions
+qwertyuiop
